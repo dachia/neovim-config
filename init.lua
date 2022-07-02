3f5d6ffdb4acb84e07bbc6b6e9c09c3328c72f41
Artur Bartkevic <artur.bartkevic@gmail.com>
2022-07-02 17:51:31 +0300

    Minimalistic fast TS setup

--- init.lua
+++ init.lua
@@ -11,209 +11,51 @@ g.maplocalleader = g.mapleader  -- map local leader key
 require("plugins")
 
 local is_win = fn.has('win32') == 1 or false
-local use_coc = false
 
-local buf_map = function(bufnr, mode, lhs, rhs, opts)
-  vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts or {
-    silent = true,
-  })
+-- LSP
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
+
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<LocalLeader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<LocalLeader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<LocalLeader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<LocalLeader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<LocalLeader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<LocalLeader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<LocalLeader>t', function ()
+    vim.lsp.buf.format { async = Tree }
+  end, bufopts)
 end
-  
 
-if use_coc then
-  g.coc_global_extensions = { 'coc-json', 'coc-tsserver', 'coc-git', 'coc-prettier', 'coc-eslint' }
-  -- Map function is not working. using vim script
-  -- map('n', '<Leader>rn', '<Plug>(coc-rename)')
-  -- map('n', '<Leader>ac', '<Plug>(coc-codeaction)')
-  -- map('n', '<leader>cl', '<Plug>(coc-codelens-action)')
-  -- Diagnostics
-  -- map('n', '[g', '<Plug>(coc-diagnostic-prev)')
-  -- map('n', ']g', '<Plug>(coc-diagnostic-next)')
-  -- Definitions
-  -- map('n', 'gd', '<Plug>(coc-definition)')
-  -- map('n', 'gy', '<Plug>(coc-type-definition)')
-  -- map('n', 'gi', '<Plug>(coc-implementation)')
-  -- map('n', 'gr', '<Plug>(coc-references)')
-  -- -- Coc-format
-  vim.cmd [[
+local lspconfig = require('lspconfig')
+g.coq_settings = { auto_start= true }
+local coq = require('coq')
 
-    " Use <c-space> to trigger completion.
-    inoremap <silent><expr> <c-space> coc#refresh()
-
-    function! s:show_documentation()
-      if (index(['vim','help'], &filetype) >= 0)
-        execute 'h '.expand('<cword>')
-      elseif (coc#rpc#ready())
-        call CocActionAsync('doHover')
-      else
-        execute '!' . &keywordprg . " " . expand('<cword>')
-      endif
-    endfunction
-
-    " Use K to show documentation in preview window.
-    nnoremap <silent> <Leader>e :call <SID>show_documentation()<CR>
+lspconfig['tsserver'].setup(coq.lsp_ensure_capabilities({
+  on_attach = on_attach,
+  flags = lsp_flags,
+}))
 
 
-    " GoTo code navigation.
-    nmap <silent> gd <Plug>(coc-definition)
-    nmap <silent> gy <Plug>(coc-type-definition)
-    nmap <silent> gi <Plug>(coc-implementation)
-    nmap <silent> gr <Plug>(coc-references)
-
-    " Diagnostics
-    nmap <silent> [g <Plug>(coc-diagnostic-prev)
-    nmap <silent> ]g <Plug>(coc-diagnostic-next)
-
-    command! -nargs=0 Prettier :CocCommand prettier.formatFile
-
-    " Highlight the symbol and its references when holding the cursor.
-    autocmd CursorHold * silent call CocActionAsync('highlight')
-
-    " Symbol renaming.
-    nmap <leader>rn <Plug>(coc-rename)
-
-    " Remap keys for applying codeAction to the current buffer.
-    nmap <leader>ac  <Plug>(coc-codeaction)
-
-    " Add `:OR` command for organize imports of the current buffer.
-    command! -nargs=0 OR   :call     CocActionAsync('runCommand', 'editor.action.organizeImport')
-
-    " Autoimport on completion
-    noremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
-
-    " Fix current line
-    nmap <leader>qf  <Plug>(coc-fix-current)
-  ]]
-  map('n', '<leader>f', ':Prettier<CR>')
-  map('n', '<leader>i', ':OR<CR>')
-else
-  -- LSP
-  local lspconfig = require'lspconfig'
-  
-  local on_attach = function(client, bufnr)
-    vim.cmd("command! LspDef lua vim.lsp.buf.definition()")
-    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
-    vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-    vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-    vim.cmd("command! LspRefs lua vim.lsp.buf.references()")
-    vim.cmd("command! LspTypeDef lua vim.lsp.buf.type_definition()")
-    vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
-    vim.cmd("command! LspDiagPrev lua vim.lsp.diagnostic.goto_prev()")
-    vim.cmd("command! LspDiagNext lua vim.lsp.diagnostic.goto_next()")
-    vim.cmd("command! LspDiagLine lua vim.lsp.diagnostic.show_line_diagnostics()")
-    vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
-    buf_map(bufnr, "n", "gd", ":LspDef<CR>")
-    buf_map(bufnr, "n", "<LocalLeader>rn", ":LspRename<CR>")
-    buf_map(bufnr, "n", "gy", ":LspTypeDef<CR>")
-    buf_map(bufnr, "n", "K", ":LspHover<CR>")
-    buf_map(bufnr, "n", "[a", ":LspDiagPrev<CR>")
-    buf_map(bufnr, "n", "]a", ":LspDiagNext<CR>")
-    buf_map(bufnr, "n", "<LocalLeader>ca", ":LspCodeAction<CR>")
-    buf_map(bufnr, "n", "<LocalLeader>e", ":LspDiagLine<CR>")
-    buf_map(bufnr, "i", "<C-k>", "<cmd> LspSignatureHelp<CR>")
-  
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-    end
-  end
-  
-  -- nvim-cmp supports additional completion capabilities
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities) 
-  
-  local debounce_text_changes = 20
-  
-  lspconfig["pyright"].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = {
-      debounce_text_changes = debounce_text_changes,
-    }
-  }
-  
-  lspconfig["tsserver"].setup {
-    on_attach = function(client, bufnr)
-      client.resolved_capabilities.document_formatting = false
-      client.resolved_capabilities.document_range_formatting = false
-      local ts_utils = require("nvim-lsp-ts-utils")
-      ts_utils.setup({
-        eslint_bin = "eslint_d",
-        eslint_enable_diagnostics = true,
-        eslint_enable_code_actions = true,
-        enable_formatting = true,
-        formatter = "prettier",
-        enable_import_on_completion = true
-      })
-      ts_utils.setup_client(client)
-      buf_map(bufnr, "n", "gs", ":TSLspOrganize<CR>")
-      buf_map(bufnr, "n", "gi", ":TSLspRenameFile<CR>")
-      buf_map(bufnr, "n", "go", ":TSLspImportAll<CR>")
-      on_attach(client, bufnr)
-    end,
-  
-    init_options = {
-      hostInfo = "neovim",
-      disableAutomaticTypingAcquisition = true,
-    },
-    capabilities = capabilities,
-    diagnosticsDelay = "500ms",
-    experimentalWatchedFileDelay = "1000ms",
-    flags = {
-      allow_incremental_sync = true,
-      debounce_text_changes = debounce_text_changes,
-    }
-  }
-  
-  local null_ls = require("null-ls")
-  null_ls.setup({
-      sources = {
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.code_actions.eslint,
-          null_ls.builtins.formatting.prettier
-      },
-      on_attach = on_attach
-  })
-
-  require "lsp_signature".setup{
-    bind = true,
-    hint_enable = false,
-    handler_opts = {border = "single"},
-    toggle_key = "<C-s>"
-    -- extra_trigger_chars = {"(", ","},
-  }
-  -- autocompletion
-  local cmp = require 'cmp'
-  cmp.setup {
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end,
-    },
-    mapping = {
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<CR>'] = cmp.mapping.confirm {
-        select = true,
-      },
-      ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
-      ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
-    },
-    sources = {
-      { name = 'nvim_lsp', max_item_count = 5 },
-      { name = "buffer", max_item_count = 5 },
-      { name = 'luasnip', max_item_count = 1 }
-    },
-    completion = {
-      keyword_length = 3
-    }
-  }
-  vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-end
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
 -- Tree sitter
 require'nvim-treesitter.configs'.setup {
+  sync_install = false,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false
@@ -257,10 +99,6 @@ require 'lualine'.setup {
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = false,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -285,8 +123,7 @@ require'nvim-tree'.setup {
   },
   view = {
     side = 'left',
-    auto_resize = true,
-    width = 50,
+    width = 60,
   }
 }
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,28 +9,12 @@ return require('packer').startup(function(use)
 
   -- language servers
   use 'neovim/nvim-lspconfig' --, commit = '96b764ba7925cfafb8346cb5afd7b128e6f8bb8a' }
-  -- typescript language server improvements
-  use 'nvim-lua/plenary.nvim'
-  use "jose-elias-alvarez/null-ls.nvim"
-  use "jose-elias-alvarez/nvim-lsp-ts-utils"
-  -- autocomplete
-  -- clide/coc.nvim', {'branch': 'release'}
-  use 'hrsh7th/cmp-buffer' -- buffer autocmp
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
-  -- signature help
-  use "ray-x/lsp_signature.nvim"
 
-  -- CoC
-  -- use { 'neoclide/coc.nvim', branch = 'master', run = 'npm i'}
+  use { 'ms-jpq/coq_nvim', branch='coq'}
+  use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
   -- tree docs
   use { 'kkoomen/vim-doge' } 
 
-  -- DB
-  use { 'tpope/vim-dadbod' }
-  use { 'kristijanhusak/vim-dadbod-ui' }
   -- Fuzzy search
   use { 'junegunn/fzf', run = function() vim.fn['fzf#install'](0) end }
   use 'junegunn/fzf.vim'
